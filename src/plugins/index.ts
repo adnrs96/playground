ffdbdcebcb0bdb4d7705c2660dc5e07614b2b946
Jean Barriere <jean@barriere.io>
2019-10-24 15:38:23 +0200

    feat(editor): put editor auto-resizable and in the playground vue

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,5 @@
 import Sentry from '@/plugins/sentry'
 import '@/plugins/monaco'
 import '@/plugins/vue-moment'
-import './monaco'
 
 export { Sentry }
--- src/views/Playground/index.vue
+++ src/views/Playground/index.vue
@@ -3,8 +3,11 @@
     <s-navbar />
     <div class="min-h-screen-no-navbar flex">
       <div class="w-2/3">
-        <br>
-        <!-- HERE GOES THE EDITOR -->
+        <monaco-editor
+          v-model="code"
+          class="w-full h-full"
+          :options="options"
+        />
       </div>
 
       <s-logs
@@ -21,15 +24,25 @@ import SNavbar from '@/components/Navbar.vue'
 import SLogs from '@/views/Playground/Logs.vue'
 import { IStorySample } from '@/models/StorySample'
 import counter from '@/samples/counter'
+import MonacoEditor from '@/components/MonacoEditor.vue'
 
 @Component({
   components: {
     SNavbar,
-    SLogs
+    SLogs,
+    MonacoEditor
   }
 })
 export default class Playground extends Vue {
   private payload: IStorySample = counter
   private automateLogs: boolean = false
+  private code: string = 'redis set key: "count" value: 0\nwhen http server listen path: "/count" as req\n  count = (redis get key: "count").result + 1\n  redis set key: "count" value: count\n  req write content: "This page has been loaded {count} times"'
+
+  private options: any = {
+    readOnly: true,
+    minimap: { enabled: false },
+    fontSize: 16,
+    automaticLayout: true
+  }
 }
 </script>
