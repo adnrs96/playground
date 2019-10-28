e88224a2caff30a192259092d64029bc52e578d7
Aurelien ARINO <aurelien.arino@gmail.com>
2019-10-28 16:48:09 +0100

    feat(functions): add submission-created function

---
+++ functions/src/submission-created.ts
@@ -0,0 +1,52 @@
+import Mailgun from 'mailgun-js'
+
+const { MG_API_KEY, MG_DOMAIN } = process.env
+
+interface IEmail {
+  from: string,
+  to: string,
+  subject: string,
+  text: string
+}
+
+const sendRegisteredInterestEmail = async ( to: string, name: string ) => {
+  return new Promise((resolve, reject) => {
+    const mailgun = Mailgun({
+      apiKey: MG_API_KEY,
+      domain: MG_DOMAIN
+    })
+    const mail: IEmail = {
+      from: 'Steve at Storyscript <steve@storyscript.io>',
+      to,
+      subject: 'Thank you for your interest',
+      text: `Thank you ${name}!`
+    }
+
+    mailgun.messages().send(mail, (err: any) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve()
+    })
+  })
+}
+
+exports.handler = async (event:  any) => {
+  try {
+    const data = JSON.parse(event.body)
+    const { email, name } = data
+    await sendRegisteredInterestEmail(email, name)
+
+    return {
+      stataudCode: 200,
+      body: `Email sent to ${name} <${email}>`
+    }
+  } catch (e) {
+    console.log(e)
+    return {
+      statusCode: 500,
+      body: e
+    }
+  }
+}
