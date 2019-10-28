60154219c5f1a6766cb29eef0da74ace6f06fbb7
Aurelien ARINO <aurelien.arino@gmail.com>
2019-10-28 13:18:36 +0100

    test(e2e): fix e2e test for login

--- tests/e2e/login.e2e.ts
+++ tests/e2e/login.e2e.ts
@@ -43,7 +43,7 @@ describe('Logs', () => {
 
     expect((await page.$$('input')).length).toEqual(3)
     expect((await page.$$eval('input', (nodes: any) => nodes.map((n:any) => n.type)))).toEqual(['text', 'text', 'email'])
-    expect((await page.$$eval('input', (nodes: any) => nodes.map((n:any) => n.name)))).toEqual(['bot-field', 'name', 'email'])
+    expect((await page.$$eval('input', (nodes: any) => nodes.map((n:any) => n.name)))).toEqual(['form-name', 'name', 'email'])
   })
 
   it('should display a some errors when I enter wrong info', async () => {
