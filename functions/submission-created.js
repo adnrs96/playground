d42470af5dc09a30376177fc3b89d3a933d8443e
Aurelien ARINO <aurelien.arino@gmail.com>
2019-10-28 17:41:13 +0100

    refactor(functions): add debug

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,7 +2,7 @@ const Mailgun = require('mailgun-js')
 
 const { MG_API_KEY, MG_DOMAIN } = process.env
 
-const sendRegisteredInterestEmail = async (to, name) => {
+const sendRegisteredInterestEmail = async (email, name) => {
   return new Promise((resolve, reject) => {
     const mailgun = Mailgun({
       apiKey: MG_API_KEY,
@@ -10,7 +10,7 @@ const sendRegisteredInterestEmail = async (to, name) => {
     })
     const mail = {
       from: 'Steve at Storyscript <steve@storyscript.io>',
-      to,
+      to: email,
       subject: 'Thank you for your interest',
       text: `Thank you for your interest ${name}!`
     }
@@ -28,7 +28,9 @@ const sendRegisteredInterestEmail = async (to, name) => {
 exports.handler = async (event) => {
   try {
     const data = JSON.parse(event.body)
+    console.log(data)
     const { email, name } = data
+    console.log(email, name)
     await sendRegisteredInterestEmail(email, name)
 
     return {
