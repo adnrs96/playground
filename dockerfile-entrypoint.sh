2a35df5fcc45e049637e380a7dab9dec7f1eba22
matthewhudson <matthud@gmail.com>
2019-10-31 13:21:15 -0400

    feat(sentry): add sentry env variable to docker config

--- dockerfile-entrypoint.sh
+++ dockerfile-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed -ir "s/DOCKER_ENV_SSL_ENABLE/$SSL_ENABLE/gm" /usr/share/nginx/html/app.js
-sed -ir "s/DOCKER_ENV_INTERCOM_APP_ID/$INTERCOM_APP_ID/gm" /usr/share/nginx/html/app.js
+sed -ir "s/DOCKER_ENV_INTERCOM_APP_ID/$INTERCOM_APP_ID/gm" /usr/share/nginx/
+sed -ir "s/DOCKER_ENV_SENTRY_DSN/$SENTRY_DSN/gm" /usr/share/nginx/
 
 nginx -g "daemon off;"
