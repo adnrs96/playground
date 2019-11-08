eca5a4651916e7a09b7f431a36561770832b1b0e
Aurelien ARINO <aurelien.arino@gmail.com>
2019-11-08 17:11:58 +0100

    feat(collaborators): animated the collaborators on hover

--- src/components/Collaborators.vue
+++ src/components/Collaborators.vue
@@ -6,17 +6,21 @@
   >
     <div
       v-for="(c, idx) in collaborators"
+      :id="`collaborator-${idx}`"
       :key="`collaborator-${idx}`"
-      class="collaborator -mr-2"
+      class="collaborator -mr-2 flex items-center transition-all-fastest"
+      @mouseenter="hoverHandler($event, true, idx)"
+      @mouseleave="hoverHandler($event, false, idx)"
     >
       <div
-        class="bg-gray-30 rounded-full border-2 border-white flex items-center justify-center shadow-md"
-        :class="{'p-1': !c.photo}"
+        class="bg-gray-30 rounded-full border-2 border-white flex items-center justify-center shadow-md z-10"
+        :class="{'p-1': !c.avatar}"
       >
         <img
-          v-if="c.photo"
-          :src="c.photo"
+          v-if="c.avatar"
+          :src="`/img/inkie-friends/${c.avatar}.png`"
           :alt="`${c.name} profile picture`"
+          :srcset="`/img/inkie-friends/${c.avatar}@2x.png 2x, /img/services//${c.avatar}@3x.png 3x`"
           width="31"
           height="31"
           class="rounded-full"
@@ -29,6 +33,14 @@
           height="23"
         />
       </div>
+      <s-text
+        v-show="hovered === idx"
+        p="3"
+        weight="medium"
+        color="text-gray-70"
+      >
+        {{ c.name }}
+      </s-text>
     </div>
   </div>
 </template>
@@ -36,13 +48,15 @@
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator'
 import { Getter } from 'vuex-class'
-import SIcon from '@/components/Icon.vue'
 import { IStoryCollaborator, IStorySample } from '../models/StorySample'
+import SIcon from '@/components/Icon.vue'
+import SText from '@/components/Text.vue'
 
 @Component({
   name: 'Collaborators',
   components: {
-    SIcon
+    SIcon,
+    SText
   }
 })
 export default class Collaborators extends Vue {
@@ -50,9 +64,22 @@ export default class Collaborators extends Vue {
   private payload!: IStorySample
 
   private collaborators: Array<IStoryCollaborator> = []
+  private hovered = -1
 
   mounted () {
     this.collaborators = this.payload.collaborators
+  }
+
+  hoverHandler (e: any, inside: boolean, idx: number) {
+    if (inside) {
+      if (idx + 1 !== this.collaborators.length) {
+        e.target.classList.remove('-mr-2')
+      }
+      this.hovered = idx
+    } else {
+      e.target.classList.add('-mr-2')
+      this.hovered = -1
+    }
   }
 }
 </script>
