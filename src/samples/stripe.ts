8ddf5e9def7cb8cc8f3aa8019e09b5b1ee31fe4f
William Martin <william.la.martin@gmail.com>
2019-11-13 19:19:19 +0100

    feat(samples): add events and comments for stripe

--- src/samples/stripe.ts
+++ src/samples/stripe.ts
@@ -15,8 +15,25 @@ const stripe: IStorySample = {
   description:
     'Inform customers that their subscriptions have been cancelled upon stripe payment failure, using the stripe, postgres and mailgun services.',
   tips: [],
-  comments: [],
-  events: []
+  comments: [{
+    author: 'Sharkie',
+    comment: 'Hey Inkie, can you explain what the psql service is?'
+  }, {
+    author: 'Inkie',
+    comment: "Totally Sharkie! psql is referencing Postgres, the database we're using to store customer information."
+  }],
+  events: [
+    {
+      title: "stripe",
+      icon: "stripe",
+      text: `  Stripe:\n    Email: steve@storyscript.io\n    Plan:  Gold\n`
+    },
+    {
+      title: "stripe",
+      icon: "stripe",
+      text: `  Stripe:\n    Email: jean@storyscript.io\n    Plan:  Silver\n`
+    },
+  ],
 }
 
 export default stripe
