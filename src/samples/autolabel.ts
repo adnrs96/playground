bf6075e86e6c8271faca901946aee5f52a385a52
William Martin <william.la.martin@gmail.com>
2019-11-13 13:51:39 +0100

    feat(samples): add autolabel comments

--- src/samples/autolabel.ts
+++ src/samples/autolabel.ts
@@ -14,7 +14,14 @@ const autolabel: IStorySample = {
   collaborators: defaultCollaborators,
   description:
     'Train a machine learning model to predict and apply labels to a gmail inbox using the gmail, machinebox, and redis services.',
-  image: 'autolabel-tweet.png'
+  image: 'autolabel-tweet.png',
+  comments: [{
+    author: 'Sharkie',
+    comment: 'Hey Inkie! Storyscript argument names are optional if the variable being provided has the same name as the function argument so I think where we have `email:email` we can use just `:email` for simplicity.'
+  }, {
+    author: 'Inkie',
+    comment: "Thanks! I'll change it soon!"
+  }]
 }
 
 export default autolabel
