029d25d22f9645caabdb323bdebe804c51df5ff3
Jean Barriere <jean@barriere.io>
2019-11-25 13:58:12 +0100

    feat(story-titles): update IStorySample + stories and welcome view
    - add id field to IStorySample
    - remove stories array from IStorySample (no files anymore)
    - update examples interfaces
    - move welcome card title to proper name
    - move link :name attr to :id
    - display story name and not filename in the example view

--- src/samples/file.ts
+++ src/samples/file.ts
@@ -3,13 +3,13 @@ import code from './file.story'
 import defaultCollaborators from './defaultCollaborators'
 
 const file: IStorySample = {
+  id: 'file',
   name: 'file',
   services: [
     'http',
     'file',
     'schedule'
   ],
-  stories: ['file'],
   code,
   collaborators: defaultCollaborators,
   description: 'An ephermal file sharing app using the schedule, file and http services.',
