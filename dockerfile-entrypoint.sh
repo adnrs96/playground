7f8a87c42c6fea85e4ee843d8fc74ad3dbf496aa
Jean Barriere <jean@barriere.io>
2019-12-16 11:51:26 +0100

    chore(docker): update build scripts

--- dockerfile-entrypoint.sh
+++ dockerfile-entrypoint.sh
@@ -3,5 +3,6 @@
 sed -ir "s/DOCKER_ENV_SSL_ENABLE/$SSL_ENABLE/gm" /usr/share/nginx/html/app.js
 sed -ir "s/DOCKER_ENV_INTERCOM_APP_ID/$INTERCOM_APP_ID/gm" /usr/share/nginx/html/app.js
 sed -ir "s/DOCKER_ENV_SENTRY_DSN/$SENTRY_DSN/gm" /usr/share/nginx/html/app.js
+sed -ir "s/DOCKER_ENV_SLS_URL/$SLS_URL/gm" /usr/share/nginx/html/app.js
 
 nginx -g "daemon off;"
