f2d2ee8725e7f5fc75f2017ab4a155e9b59ed4f9
Aditya Bansal <adi.bansal241996@gmail.com>
2019-12-20 14:27:28 +0530

    tests(InfosPane): add local store to unit tests.

--- tests/unit/views/Editor/InfosPane.spec.ts
+++ tests/unit/views/Editor/InfosPane.spec.ts
@@ -1,8 +1,14 @@
-import { Wrapper, shallowMount } from '@vue/test-utils'
+import { Wrapper, shallowMount, createLocalVue } from '@vue/test-utils'
 import InfosPane from '@app/Editor/InfosPane.vue'
+import EditorErrors from '@app/store/modules/EditorErrors'
+import Vuex, { Store } from 'vuex'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
 describe('InfosPane.vue', () => {
   let infos: Wrapper<InfosPane>
+  const store: Store<any> = new Vuex.Store(EditorErrors)
 
   beforeEach(() => {
     infos = shallowMount(InfosPane, {
@@ -10,7 +16,9 @@ describe('InfosPane.vue', () => {
         's-text': '<div />',
         's-icon': '<div />',
         's-scrollbar': '<div />'
-      }
+      },
+      localVue,
+      store
     })
   })
 
