a5ddcb2c9de7014da5b8190430440b38480663bd
Aditya Bansal <adi.bansal241996@gmail.com>
2019-12-20 11:14:02 +0530

    chore(plugin-error): add code inline documentation.

--- src/editor/plugin-error/index.ts
+++ src/editor/plugin-error/index.ts
@@ -11,11 +11,36 @@ export class StoryscriptError implements EditorPlugin {
     private readonly setModelMarkersOriginal: ISetModelMarkers
 
     constructor (monacoEditor: typeof editor) {
+      // We need this monacoEditor instead of the one which the attach
+      // function is called with because these two are different.
+      // The one inside attach function call parameters is implementing
+      // IStandaloneCodeEditor interface whereas editor in this constructor
+      // is actually a module.
+      // We need this editor because it has some functions which aren't present
+      // inside the IStandalongCodeEditor (like setModelMarkers) which we need
+      // to override in attach. One can think of editor in this constructor as
+      // a module with some free floating functions which performs some tasks
+      // on parameters provided to them whereas the editor inside the attach
+      // function is more of an instance of the CodeEditor which one of the
+      // free floating function (i.e. create()) inside this editor created.
       this.monacoEditor = monacoEditor
       this.setModelMarkersOriginal = monacoEditor.setModelMarkers
     }
 
     public attach (editor: IStandaloneCodeEditor) {
+      // NOTE: Overhere we are overriding/swapping out a function
+      // from monaco library with our own version. This is needed
+      // for displaying errors as per Storyscript Studio design.
+      // SLS plugin makese use of a library monaco-languageclient
+      // which handles most of the RPC stuff for the SLS plugin and
+      // is also responsible for capturing the error diagnostics from
+      // SLS server and reporting/decorating the code with appropriate
+      // Redflags and Peek problem context widgets. It does all this by
+      // making a call to the `setModelMarkers` function of the monaco
+      // library directly. Thus in order to intercept that call we need
+      // to swap out the implementation of the function with our own and
+      // extract data from the IMarkerData and use it to display #studio
+      // style errors.
       this.monacoEditor.setModelMarkers = function (model: editor.ITextModel, owner: string, markers: editor.IMarkerData[]): void {
         if (markers.length > 0) {
           const sln = markers[0].startLineNumber
@@ -32,6 +57,7 @@ export class StoryscriptError implements EditorPlugin {
     }
 
     public detach () {
+      // Restore the function definition with monaco's original version.
       this.monacoEditor.setModelMarkers = this.setModelMarkersOriginal
     }
 }
